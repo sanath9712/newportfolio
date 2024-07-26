52e9a131c3ee8b0713c7318e04b3b87b873db61e
sanathbhimsen <sanathbhimsen26@gmail.com>
2024-07-26 11:24:55 -0400

    fixed the build failures

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,14 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 import { ThemeProvider } from "../Components/ThemeContext"; // Adjust the path as needed
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider>
+      <Head>
+        <title>Sanath Bhimsen</title>
+      </Head>
       <Component {...pageProps} />
     </ThemeProvider>
   );
